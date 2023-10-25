ac39e4d71503509a14f767fbef0550db9223b84f
Dani-ctrl-n <mugabodaniel26@gmail.com>
2023-10-25 12:05:47 +0200

    create sign in api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 
+const app = express();
+
 dotenv.config();
 
 mongoose
@@ -15,13 +17,7 @@ mongoose
     console.log(err);
   });
 
-const app = express();
-
 app.use(express.json());
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
@@ -32,4 +28,8 @@ app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
   return res.status(statusCode).json({ success: false, statusCode, message });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
