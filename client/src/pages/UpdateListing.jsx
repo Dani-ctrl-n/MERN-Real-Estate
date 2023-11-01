a776c02836dfbdb105240e51aa810645560030f8
Dani-ctrl-n <mugabodaniel26@gmail.com>
2023-11-01 14:38:14 +0200

    complete listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -296,8 +296,10 @@ export default function CreateListing() {
                 value={formData.regularPrice}
               />
               <div className="flex flex-col items-center">
-                <p>Regular price</p>
-                <span className="text-xs">($/month)</span>
+                <p>Regular price ($)</p>
+                {formData.type === "rent" && (
+                  <span className="text-xs">( per month )</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -312,8 +314,10 @@ export default function CreateListing() {
                   value={formData.discountPrice}
                 />
                 <div className="flex flex-col items-center">
-                  <p>Discounted price</p>
-                  <span className="text-xs">($/month)</span>
+                  <p>Discounted price ($)</p>
+                  {formData.type === "rent" && (
+                    <span className="text-xs">( per month )</span>
+                  )}
                 </div>
               </div>
             )}
